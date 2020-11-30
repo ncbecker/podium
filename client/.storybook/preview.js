607567daca084cb04a40ff7c4c83cf6371421fa2
ncbecker <nbecker.ac@gmail.com>
2020-11-30 16:59:08 +0100

    :lipstick: Add GlobalStyle to Storybook

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,3 +1,14 @@
+import GlobalStyle from "../src/GlobalStyle";
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
+];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 };
