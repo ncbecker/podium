a5c3c9cf29983cd8fbe8c3d8ed8c216560831793
ncbecker <nbecker.ac@gmail.com>
2020-12-01 13:50:06 +0100

    :sparkles: Create Icon Button components and stories

---
+++ client/src/components/Button/IconButton.js
@@ -0,0 +1,94 @@
+import styled from "styled-components/macro";
+import PropTypes from "prop-types";
+import { ReactComponent as ThemeToggler } from "../../assets/icon-sun-empty.svg";
+import { ReactComponent as BurgerMenu } from "../../assets/icon-burger-menu.svg";
+import { ReactComponent as Close } from "../../assets/icon-close.svg";
+import { ReactComponent as ArrowBack } from "../../assets/icon-arrow-back.svg";
+import { ReactComponent as LogOut } from "../../assets/icon-logout.svg";
+import { ReactComponent as Filter } from "../../assets/icon-filter.svg";
+import { ReactComponent as Info } from "../../assets/icon-info-full.svg";
+import { ReactComponent as AddToSpotify } from "../../assets/add-to-spotify.svg";
+
+export const IconButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: ${(props) => (props.nav ? "48px" : "auto")};
+  height: ${(props) => (props.nav ? "48px" : "auto")};
+  svg {
+    fill: var(--icon-light);
+  }
+`;
+
+export const ThemeTogglerButton = () => {
+  return (
+    <IconButton nav>
+      <ThemeToggler />
+    </IconButton>
+  );
+};
+
+export const BurgerMenuButton = () => {
+  return (
+    <IconButton nav>
+      <BurgerMenu />
+    </IconButton>
+  );
+};
+
+export const CloseButton = () => {
+  return (
+    <IconButton nav>
+      <Close />
+    </IconButton>
+  );
+};
+
+export const ArrowBackButton = () => {
+  return (
+    <IconButton nav>
+      <ArrowBack />
+    </IconButton>
+  );
+};
+
+export const LogOutButton = () => {
+  return (
+    <IconButton nav>
+      <LogOut />
+    </IconButton>
+  );
+};
+
+export const FilterButton = () => {
+  return (
+    <IconButton nav>
+      <Filter />
+    </IconButton>
+  );
+};
+
+export const InfoButton = () => {
+  return (
+    <IconButton nav>
+      <Info />
+    </IconButton>
+  );
+};
+
+export const AddToSpotifyButton = () => {
+  return (
+    <IconButton nav>
+      <AddToSpotify />
+    </IconButton>
+  );
+};
+
+IconButton.propTypes = {
+  nav: PropTypes.bool,
+  onClick: PropTypes.func,
+};
+
+IconButton.defaultProps = {
+  onClick: undefined,
+};
