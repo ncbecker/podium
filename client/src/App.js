0d7aa8d2e93c5be8553657053d5ac8b402680391
ncbecker <nbecker.ac@gmail.com>
2020-12-02 10:50:04 +0100

    :construction: Add AppWrapper to center Page (work in progress)

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
+import styled from "styled-components";
 import GlobalStyle from "./GlobalStyle";
 import LogInPage from "./pages/Page/LogInPage";
 
+const AppWrapper = styled.div`
+  margin: 0 auto;
+`;
+
 function App() {
   return (
-    <div className="App">
+    <AppWrapper>
       <GlobalStyle />
       <LogInPage />
-    </div>
+    </AppWrapper>
   );
 }
 
