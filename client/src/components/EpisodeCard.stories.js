b2221ca5ca398e28d7c08aa582af46cb70d27411
ncbecker <nbecker.ac@gmail.com>
2020-12-03 00:19:53 +0100

    :sparkles: Create story for episode card

---
+++ client/src/components/EpisodeCard.stories.js
@@ -0,0 +1,9 @@
+import { EpisodeCard } from "./EpisodeCard";
+
+export default {
+  title: "Podium/Cards",
+  parameters: { layout: "centered" },
+  component: EpisodeCard,
+};
+
+export const Episode = () => <EpisodeCard />;
