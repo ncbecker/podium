5a4ff1b7a88b60ef05a3385022aae50148e450e2
ncbecker <nbecker.ac@gmail.com>
2020-12-03 01:04:54 +0100

    :sparkles: Create search input field for voting page

---
+++ client/src/components/EpisodeSearch.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import styled from "styled-components/macro";
+import PropTypes from "prop-types";
+
+const InputField = styled.input`
+  width: 280px;
+  height: 45px;
+  padding: 0 20px;
+  background: var(--input-overlay-light);
+  border: none;
+  outline: none;
+  border-radius: 30px;
+`;
+
+export const EpisodeSearch = () => {
+  const [searchData, setSearchData] = useState("");
+
+  const handleChange = (event) => {
+    setSearchData(event.target.value);
+    console.log(searchData);
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <InputField
+        type="text"
+        placeholder="Search your favorite episodes..."
+        name="episode"
+        value={searchData}
+        onChange={handleChange}
+        required
+      />
+    </form>
+  );
+};
+
+InputField.propTypes = {
+  type: PropTypes.string,
+  placeholder: PropTypes.string,
+  name: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
