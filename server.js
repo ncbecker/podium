33dd8787f78eb46a1daf80e948da8e71fe376e19
ncbecker <nbecker.ac@gmail.com>
2020-12-15 14:58:10 +0100

    :sparkles: Setup server-side Auth Code Flow from Spotify API
    https://developer.spotify.com/documentation/general/guides/authorization-guide/#authorization-code-flow

--- server.js
+++ server.js
@@ -1,15 +1,80 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const { getEpisodeInfo } = require("./lib/SpotifyMethods");
-const { getAppAccessToken } = require("./lib/SpotifyAccess");
+const cookieParser = require("cookie-parser");
+const querystring = require("querystring");
+
+const {
+  getEpisodeInfo,
+  getCurrentUserProfile,
+} = require("./lib/SpotifyMethods");
+const {
+  getAppAccessToken,
+  getUserAccessToken,
+  refreshUserAccessToken,
+} = require("./lib/SpotifyAccess");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/oauth/apptoken", async (request, response) => {
-  const apptoken = await getAppAccessToken();
-  response.send(apptoken);
+app.use(cookieParser());
+
+// OAuth Spotify
+
+app.get("/oauth/spotify/authorize", (request, response) => {
+  const url =
+    "https://accounts.spotify.com/authorize?" +
+    querystring.stringify({
+      response_type: "code",
+      client_id: process.env.CLIENT_ID,
+      scope: "user-read-private user-read-email",
+      redirect_uri: process.env.REDIRECT_URI,
+    });
+  response.redirect(url);
+});
+
+app.get("/oauth/spotify/validate", async (request, response) => {
+  const { code } = request.query;
+  const {
+    access_token: accessToken,
+    expires_in: expiresIn,
+    refresh_token: refreshToken,
+  } = await getUserAccessToken(code);
+  response.setHeader(
+    "Set-Cookie",
+    `access=${JSON.stringify({
+      accessToken,
+      refreshToken,
+    })};path=/;Max-Age=${expiresIn}`
+  );
+  response.send("Authorized. You can close this window :)");
+});
+
+app.get("/oauth/spotify/apptoken", async (request, response) => {
+  const appToken = await getAppAccessToken();
+  response.send(appToken);
+});
+
+app.get("/oauth/spotify/usertoken", async (request, response) => {
+  const authCode = request.headers.authorization;
+  const userToken = await getUserAccessToken(authCode);
+  response.send(userToken);
+});
+
+app.get("/oauth/spotify/refreshtoken", async (request, response) => {
+  const refreshToken = request.headers.authorization;
+  const refreshedAccessToken = await refreshUserAccessToken(refreshToken);
+  response.send(refreshedAccessToken);
+});
+
+// Spotify API
+
+app.get("/api/user/profile", async (request, response) => {
+  const { access } = request.cookies;
+  const { accessToken } = JSON.parse(access);
+
+  const userProfileData = await getCurrentUserProfile(accessToken);
+  response.send(userProfileData);
 });
 
 app.get("/api/episode/:id", async (request, response) => {
