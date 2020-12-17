5a281be2076caa04e47f0ee3bf89d8416809a61a
ncbecker <nbecker.ac@gmail.com>
2020-12-17 16:26:31 +0100

    :sparkles: Create search suggestions component

--- client/src/components/EpisodeSearch.js
+++ client/src/components/EpisodeSearch.js
@@ -12,22 +12,76 @@ const InputField = styled.input`
   border-radius: 30px;
 `;
 
-export const EpisodeSearch = ({ value, onChange }) => {
+const Container = styled.div`
+  position: relative;
+`;
+
+const SuggestionsContainer = styled.div`
+  position: absolute;
+  top: 65px;
+  width: 250px;
+  padding: 5px 5px 0 5px;
+  background: var(--input-overlay-light);
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const SearchSuggestion = styled.div`
+  margin-bottom: 5px;
+  padding: 5px;
+  background: var(--info-bubble-light);
+  border-radius: 5px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  span {
+    max-width: 240px;
+    font-size: 0.75rem;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    pointer-events: none;
+  }
+  img {
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    justify-self: flex-end;
+    pointer-events: none;
+  }
+`;
+
+export const EpisodeSearch = ({ value, onChange, suggestions, onClick }) => {
   return (
-    <InputField
-      type="text"
-      placeholder="Search your favorite episodes..."
-      name="episode"
-      value={value}
-      onChange={onChange}
-      required
-    />
+    <Container>
+      <InputField
+        type="text"
+        placeholder="Search your favorite episodes..."
+        name="episode"
+        value={value}
+        onChange={onChange}
+        required
+      />
+      {suggestions && (
+        <SuggestionsContainer>
+          {suggestions?.map((episode) => (
+            <SearchSuggestion key={episode.id} onClick={onClick}>
+              <span>{episode.name}</span>
+              <img src={episode.images[2].url} alt={episode.name} />
+            </SearchSuggestion>
+          ))}
+        </SuggestionsContainer>
+      )}
+    </Container>
   );
 };
 
 EpisodeSearch.propTypes = {
   value: PropTypes.string,
   onChange: PropTypes.func,
+  suggestions: PropTypes.array,
+  onClick: PropTypes.func,
 };
 
 InputField.propTypes = {
