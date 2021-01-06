ff3187b1680d68cb3f49c1ec1a34689cb67de60e
ncbecker <nbecker.ac@gmail.com>
2021-01-06 14:31:47 +0100

    :sparkles: Add ThemeProvider and theme state to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
-import styled from "styled-components";
+import { useState } from "react";
+import styled from "styled-components/macro";
+import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./GlobalStyle";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
@@ -8,6 +10,7 @@ import VotingPage from "./pages/VotingPage";
 import EpisodeDetailsPage from "./pages/EpisodeDetailsPage";
 import UserPage from "./pages/UserPage";
 import MenuLogInPage from "./pages/MenuLogInPage";
+import { dark, light } from "./utils/theme";
 
 const AppWrapper = styled.div`
   max-width: 375px;
@@ -17,25 +20,33 @@ const AppWrapper = styled.div`
 `;
 
 function App() {
+  const [theme, setTheme] = useState(light);
+
+  const handleChangeTheme = () => {
+    setTheme(theme === dark ? light : dark);
+  };
+
   return (
     <Router>
       <AuthProvider>
-        <AppWrapper>
-          <GlobalStyle />
-          <Switch>
-            <Route exact path="/">
-              <LogInPage />
-            </Route>
-            <Route path="/vote">
-              <VotingPage />
-            </Route>
-            <ProtectedRoute path="/user" component={UserPage} />
-            <ProtectedRoute path="/login" component={MenuLogInPage} />
-            <Route path="/details/:id">
-              <EpisodeDetailsPage />
-            </Route>
-          </Switch>
-        </AppWrapper>
+        <ThemeProvider theme={theme}>
+          <AppWrapper>
+            <GlobalStyle />
+            <Switch>
+              <Route exact path="/">
+                <LogInPage toggleTheme={handleChangeTheme} />
+              </Route>
+              <Route path="/vote">
+                <VotingPage />
+              </Route>
+              <ProtectedRoute path="/user" component={UserPage} />
+              <ProtectedRoute path="/login" component={MenuLogInPage} />
+              <Route path="/details/:id">
+                <EpisodeDetailsPage />
+              </Route>
+            </Switch>
+          </AppWrapper>
+        </ThemeProvider>
       </AuthProvider>
     </Router>
   );
