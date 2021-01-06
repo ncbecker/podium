5e2c9b60522b94df275579dedfd55bf37f7c4144
ncbecker <nbecker.ac@gmail.com>
2021-01-06 19:12:11 +0100

    :bug: Fix createDevUrl

--- setupProxy.js
+++ setupProxy.js
@@ -2,10 +2,11 @@ require("dotenv").config();
 
 const createDevUrl = () => {
   let devUrl = { auth: "/oauth/spotify/authorize", vote: "/vote" };
-  if (process.env.DEV_PROXY_URL) {
+  if (process.env.DEV_PROXY_URL === "dev") {
     devUrl.auth = "http://localhost:3001/oauth/spotify/authorize";
     devUrl.vote = "http://localhost:3000/vote";
   }
+  console.log({ devUrl });
   return devUrl;
 };
 
