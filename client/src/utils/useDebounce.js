029827ed3e0dbc0422e70a0f97d76eef8d18bc69
ncbecker <nbecker.ac@gmail.com>
2021-01-28 18:16:22 +0100

    :sparkles: Add custom hook for search debounce

---
+++ client/src/utils/useDebounce.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [delay, value]);
+
+  return debouncedValue;
+};
+
+export default useDebounce;
